bd6409941dc246764b8c41562b088d57460d6c12
al-varo <52884208+al-varo@users.noreply.github.com>
2024-10-04 02:51:42 +0700

    Update rin.py

--- rin.py
+++ rin.py
@@ -299,7 +299,7 @@ def get_product_in(nama):
             for row in record:
                 produk = row[0]
                 qty = row[1]
-                textpre=textpre+"{}\t{}\n".format(produk, ribuan(qty))
+                textpre=textpre+"{0:<5} {1}".format(produk, ribuan(qty))+"\n"
             text="""
 ```
 {}
