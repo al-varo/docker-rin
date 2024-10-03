f3ebaa1633116d79ed6d64dd8a56d4f46cf0be69
al-varo <52884208+al-varo@users.noreply.github.com>
2024-10-04 01:51:30 +0700

    Update rin.py

--- rin.py
+++ rin.py
@@ -23,7 +23,7 @@ TIMEOUT = 3
 RETRY = 1
 global_err=""
 
-sql_in = "SELECT (SELECT name FROM product_product where product_tmpl_id=sm.product_id), product_uos_qty from stock_move sm where date >= '2024-10-03' and origin like 'PO%' and sm.name not like 'ROKOK %' order by nama asc;"
+sql_in = "SELECT (SELECT name FROM product_product where product_tmpl_id=sm.product_id) as nama, product_uos_qty from stock_move sm where date >= '2024-10-03' and origin like 'PO%' and sm.name not like 'ROKOK %' order by nama asc;"
 sql_draft = "SELECT \
             (SELECT name FROM res_partner WHERE id = ai.partner_id), \
             date_invoice, \
